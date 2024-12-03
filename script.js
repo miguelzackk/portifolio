a1dddd1d3874ad1b88fa79dbe83e1ab5d3c55420
“miguelzackk” <mzsilvaw@gmail.com>
2024-12-02 22:26:00 -0300

    init menu hamburguer

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function toggleMenu() {
-    const sideMenu = document.getElementById("sideMenu");
-    const hamburger = document.querySelector(".hamburger");
+  const sideMenu = document.getElementById("sideMenu");
+  const hamburger = document.querySelector(".hamburger");
 
-    // Ativa/desativa o menu
-    sideMenu.classList.toggle("active");
-    hamburger.classList.toggle("active");
-  }
+  sideMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+}
