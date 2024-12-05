aa8a8c62d643d785f1db7887157857237fa25adc
“miguelzackk” <mzsilvaw@gmail.com>
2024-12-04 21:51:34 -0300

    finish-portfolio-v1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,3 +5,34 @@ function toggleMenu() {
   sideMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
 }
+
+document.querySelectorAll(".menu-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    const sideMenu = document.getElementById("sideMenu");
+    const hamburger = document.querySelector(".hamburger");
+
+    sideMenu.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("section-visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  sections.forEach((section) => {
+    section.classList.add("section-hidden");
+    observer.observe(section);
+  });
+});
